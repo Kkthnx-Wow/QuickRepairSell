d7a07f34b74512a4b34b52dcc6ea8029afa25d3f
Kkthnx <JRussell20@gmail.com>
2023-02-19 22:47:50 -0500

    Code stuff testing

--- QuickRepairSell/Repair.lua
+++ QuickRepairSell/Repair.lua
@@ -19,31 +19,40 @@ local IsShiftKeyDown = IsShiftKeyDown
 local LE_GAME_ERR_GUILD_NOT_ENOUGH_MONEY = LE_GAME_ERR_GUILD_NOT_ENOUGH_MONEY
 
 -- Auto repair
-local autoRepair -- function that handles the repair of all items
-local canRepair -- boolean indicating if repair is possible
+-- local autoRepair -- function that handles the repair of all items
+-- local canRepair -- boolean indicating if repair is possible
 local isBankEmpty -- boolean indicating if guild bank is empty
 local isShown -- boolean indicating if the function is currently shown
 local repairAllCost -- cost to repair all items
 
-local AutoRepair = {
-	NONE = false,
-	GUILD = true,
-	PLAYER = true,
+local stringFormat = string.format
+local cTimerAfter = C_Timer.After
+local canGuildBankRepair = CanGuildBankRepair
+local canMerchantRepair = CanMerchantRepair
+local getGuildBankWithdrawMoney = GetGuildBankWithdrawMoney
+local getMoney = GetMoney
+local getRepairAllCost = GetRepairAllCost
+local isShiftKeyDown = IsShiftKeyDown
+local isInGuild = IsInGuild
+local leGameErrGuildNotEnoughMoney = LE_GAME_ERR_GUILD_NOT_ENOUGH_MONEY
+local repairAllItems = _G.RepairAllItems
+
+local autoRepairOptions = {
+	none = false,
+	guild = true,
+	player = true,
 }
 
--- We have to fix this
-local YELLOW_FONT_COLOR_CODE = "|cffffff00"
-local GOLD_COLOR = "|cffffd700"
-local SILVER_COLOR = "|cffd0d0d0"
-local COPPER_COLOR = "|cffc77050"
-local GOLD_AMOUNT_SYMBOL = GOLD_COLOR .. GOLD_AMOUNT_SYMBOL .. "|r"
-local SILVER_AMOUNT_SYMBOL = SILVER_COLOR .. SILVER_AMOUNT_SYMBOL .. "|r"
-local COPPER_AMOUNT_SYMBOL = COPPER_COLOR .. COPPER_AMOUNT_SYMBOL .. "|r"
+local moneyFormat = {
+	gold = "|cffffd700%s|r",
+	silver = "|cffd0d0d0%s|r",
+	copper = "|cffc77050%s|r",
+}
 
-local function FormatMoneyString(money, shortFormat)
+local function formatMoneyString(money, shortFormat)
 	if money >= 1e6 and not shortFormat then
-		local formattedMoney = BreakUpLargeNumbers(format("%d", money / 1e4))
-		return formattedMoney .. GOLD_AMOUNT_SYMBOL
+		local formattedMoney = BreakUpLargeNumbers(stringFormat("%d", money / 1e4))
+		return stringFormat("%s%s", formattedMoney, moneyFormat.gold)
 	else
 		if money > 0 then
 			local moneyString = ""
@@ -52,113 +61,102 @@ local function FormatMoneyString(money, shortFormat)
 			local copper = money % 100
 
 			if gold > 0 then
-				moneyString = " " .. gold .. GOLD_AMOUNT_SYMBOL
+				moneyString = stringFormat(" %d%s", gold, moneyFormat.gold)
 			end
 
 			if silver > 0 then
-				moneyString = moneyString .. " " .. silver .. SILVER_AMOUNT_SYMBOL
+				moneyString = stringFormat("%s %d%s", moneyString, silver, moneyFormat.silver)
 			end
 
 			if copper > 0 then
-				moneyString = moneyString .. " " .. copper .. COPPER_AMOUNT_SYMBOL
+				moneyString = stringFormat("%s %d%s", moneyString, copper, moneyFormat.copper)
 			end
 
 			return moneyString
 		else
-			return " 0" .. COPPER_AMOUNT_SYMBOL
+			return stringFormat(" 0%s", moneyFormat.copper)
 		end
 	end
 end
 
 local function delayFunc()
-	-- Check if the guild bank is empty
 	if isBankEmpty then
-		-- Call the autoRepair function with the override argument set to true
-		autoRepair(true)
+		repairAllItems(true)
 	else
-		-- Print a message indicating that the repair was done with the guild bank
-		print(string_format("%s%s", YELLOW_FONT_COLOR_CODE, "Your items have been repaired using guild bank funds for: ", FormatMoneyString(repairAllCost)))
+		print(
+			stringFormat(
+				"|cffffff00Your items have been repaired using guild bank funds for: %s|r",
+				formatMoneyString(repairAllCost)
+			)
+		)
 	end
 end
 
-function autoRepair(override)
-	-- If the function is already shown and override is not set, return immediately
+local function autoRepair(override)
 	if isShown and not override then
 		return
 	end
 
-	-- set isShown to true and isBankEmpty to false
 	isShown = true
 	isBankEmpty = false
 
-	-- Get the player's current money
-	local myMoney = GetMoney()
+	local myMoney = getMoney()
 
-	-- Get the cost to repair all items and check if repair is possible
-	repairAllCost, canRepair = GetRepairAllCost()
+	local repairAllCost, canRepair = getRepairAllCost()
 
-	-- If repair is possible and there is a cost to repair
 	if canRepair and repairAllCost > 0 then
-		-- If override is not set, check if C["Inventory"].AutoRepair.Value is 1 and the player is in a guild and the guild bank can repair
-		if not override and AutoRepair.GUILD and IsInGuild() and CanGuildBankRepair() and GetGuildBankWithdrawMoney() >= repairAllCost then
-			_G.RepairAllItems(true)
+		if
+			not override
+			and autoRepairOptions.guild
+			and isInGuild()
+			and canGuildBankRepair()
+			and getGuildBankWithdrawMoney() >= repairAllCost
+		then
+			repairAllItems(true)
 		else
-			-- If the player has enough money, repair the items and print a message
 			if myMoney > repairAllCost then
-				_G.RepairAllItems()
-				print(string_format("%s%s", YELLOW_FONT_COLOR_CODE, "Your items have been repaired for:|r ", FormatMoneyString(repairAllCost)))
-				return
+				repairAllItems()
+				print(
+					stringFormat("|cffffff00Your items have been repaired for:|r %s", formatMoneyString(repairAllCost))
+				)
 			else
-				-- If the player doesn't have enough money, print a message
-				print("You don't have enough money to repair,|r " .. UnitName("player"))
-				return
+				print(stringFormat("You don't have enough money to repair,|r %s", UnitName("player")))
 			end
 		end
 
-		-- Wait 0.5 seconds before calling delayFunc
-		C_Timer_After(0.5, delayFunc)
+		cTimerAfter(0.5, delayFunc)
 	end
 end
 
 local function checkBankFund(_, msgType)
-	-- Check if the message type is indicating that the guild doesn't have enough money
-	if msgType == LE_GAME_ERR_GUILD_NOT_ENOUGH_MONEY then
-		-- Set the isBankEmpty variable to true
+	if msgType == leGameErrGuildNotEnoughMoney then
 		isBankEmpty = true
 	end
 end
 
 local function merchantClose()
-	-- Set isShown to false
 	isShown = false
-	-- Unregister the UI_ERROR_MESSAGE event
 	frame:UnregisterEvent("UI_ERROR_MESSAGE")
-	-- Unregister the MERCHANT_CLOSED event
 	frame:UnregisterEvent("MERCHANT_CLOSED")
 end
 
 local function merchantShow()
-	-- If shift key is down or C["Inventory"].AutoRepair.Value is 0 or the merchant can't repair, return
-	if IsShiftKeyDown() or AutoRepair.NONE or not CanMerchantRepair() then
+	if isShiftKeyDown() or autoRepairOptions.none or not canMerchantRepair() then
 		return
 	end
 
-	-- Call the autoRepair function
 	autoRepair()
-	-- Register the UI_ERROR_MESSAGE event
 	frame:RegisterEvent("UI_ERROR_MESSAGE")
-	-- Register the MERCHANT_CLOSED event
 	frame:RegisterEvent("MERCHANT_CLOSED")
 end
 
-local function CreateAutoRepair()
-	-- Register the MERCHANT_SHOW event
+local function createAutoRepair()
 	frame:RegisterEvent("MERCHANT_SHOW")
 end
 
 function frame:OnEvent(event, ...)
 	if event == "PLAYER_LOGIN" then
-		CreateAutoRepair()
+		createAutoRepair()
 	elseif event == "MERCHANT_SHOW" then
 		merchantShow()
 	elseif event == "MERCHANT_CLOSED" then
--- QuickRepairSell/Sell.lua
+++ QuickRepairSell/Sell.lua
@@ -31,28 +31,20 @@ local isKnownString = {
 	[TRANSMOGRIFY_TOOLTIP_ITEM_UNKNOWN_APPEARANCE_KNOWN] = true,
 }
 
--- function to check if the transmog appearance is unknown
 local function IsUnknownTransmog(bagID, slotID)
-	-- retrieve the data of the item in the specified bag and slot
-	local data = C_TooltipInfo.GetBagItem(bagID, slotID)
-	-- check if the data is valid and the line data is present
-	local lineData = data and data.lines
-	if not lineData then
+	local tooltipData = C_TooltipInfo.GetBagItem(bagID, slotID)
+	if not tooltipData or not tooltipData.lines then
 		return
 	end
 
-	-- loop through the line data in reverse order
-	for i = #lineData, 1, -1 do
-		local line = lineData[i]
-		-- check if the line has arguments
+	for i = #tooltipData.lines, 1, -1 do
+		local line = tooltipData.lines[i]
 		if line and line.args then
-			local args = line.args
-			-- check if the fourth argument is present and its field is "price"
-			if args[4] and args[4].field == "price" then
+			local argAppearanceString = line.args[2] and line.args[2].stringVal
+			local argPrice = line.args[4] and line.args[4].field == "price"
+			if argPrice then
 				return false
-			end
-			-- check if the second argument is present and it's value is in the known string values table
-			if args[2] and isKnownString[args[2].stringVal] then
+			elseif argAppearanceString and isKnownString[argAppearanceString] then
 				return true
 			end
 		end
@@ -81,8 +73,16 @@ local function startSelling()
 			-- get information about the item in the current slot
 			local info = C_Container_GetContainerItemInfo(bag, slot)
 			if info then
-				if not cache["b" .. bag .. "s" .. slot] and info.hyperlink and not info.hasNoValue and (info.quality == 0 and not petTrashCurrenies[info.itemID] and (not C_TransmogCollection_GetItemInfo(info.hyperlink) or not IsUnknownTransmog(bag, slot))) then
-					print("Item eligible for selling: " .. info.hyperlink)
+				if
+					not cache["b" .. bag .. "s" .. slot]
+					and info.hyperlink
+					and not info.hasNoValue
+					and (
+						info.quality == 0
+						and not petTrashCurrenies[info.itemID]
+						and (not C_TransmogCollection_GetItemInfo(info.hyperlink) or not IsUnknownTransmog(bag, slot))
+					)
+				then
 					cache["b" .. bag .. "s" .. slot] = true
 					C_Container_UseContainerItem(bag, slot)
 					C_Timer_After(0.15, startSelling)
@@ -94,19 +94,15 @@ local function startSelling()
 end
 
 local function updateSelling(event, ...)
-	-- exit if AutoSell feature is not enabled
-	-- if not AutoSell.enable then
-	--     return
-	-- end
-
 	local _, arg = ...
 	if event == "MERCHANT_SHOW" then
 		-- exit if shift key is pressed
-		if IsShiftKeyDown() then
+		local shiftKeyDown = IsShiftKeyDown()
+		if shiftKeyDown then
 			return
 		end
 
-		-- set stop flag to false and clear cache table
+		-- start selling items
 		stop = false
 		table_wipe(cache)
 		-- start selling items
